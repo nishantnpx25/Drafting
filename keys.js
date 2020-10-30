4fac1fac06e7efa3dd989c556f853454466063f3
Nishant Pandey <pnishant0725@gmail.com>
2020-10-30 13:02:00 +0530

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-//connection with mongodb
-//creating json web token
-
-module.exports = {
-  MONGOURI:
-    "mongodb+srv://nishant:RVqgTUsJClasKcFK@cluster0.8mwg9.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  JWT_SECRET: "vfsgbfrhgfdhgdsh5657htgf",
-};
